8bd036ea640e9a676fd3445505366d451fea8d41
Nataliya R <n.rudenko@gmail.com>
2019-04-07 21:46:43 +0000

    add iteration

--- lib/museum/scraper.rb
+++ lib/museum/scraper.rb
@@ -3,7 +3,10 @@ class Scraper
   def scrape_page
     html = open("https://www.whitney.org/exhibitions")
     doc = Nokogiri::HTML(html)
+    doc.css("div.exhibitions__featured-list").each do |exhibition|
+      
     binding.pry
-  end 
+  end
+end 
   
 end 
