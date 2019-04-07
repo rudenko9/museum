eb4c193016230304d1f691beed8085217c313eb3
Nataliya R <n.rudenko@gmail.com>
2019-04-07 18:54:40 +0000

    add relative_require

---
+++ lib/environment.rb
@@ -0,0 +1,12 @@
+#require "museum/version"
+require_relative './museum/version'
+require_relative './museum/cli'
+
+require 'pry'
+require 'nokogiri'
+
+
+module Museum
+  class Error < StandardError; end
+  # Your code goes here...
+end
--- lib/museum.rb
+++
@@ -1,6 +0,0 @@
-require "museum/version"
-
-module Museum
-  class Error < StandardError; end
-  # Your code goes here...
-end
