790110f10f58075b7462a7eaab60fc48053043a4
Nataliya R <n.rudenko@gmail.com>
2019-04-07 22:24:32 +0000

    add

--- lib/environment.rb
+++ lib/environment.rb
@@ -6,6 +6,8 @@ require 'open-uri'
 require_relative './museum/version'
 require_relative './museum/cli'
 require_relative './museum/scraper'
+require_relative './museum/exhibition'
+
 
 
 module Museum
