be64f69a1d5331dcc4f1e4a940c130384aac8627
Nataliya R <n.rudenko@gmail.com>
2019-04-08 15:38:15 +0000

    add more  scraping

--- lib/museum/scraper.rb
+++ lib/museum/scraper.rb
@@ -1,15 +1,22 @@
 class Scraper 
   
   def scrape_page
-    #html = open("https://www.whitney.org/exhibitions")
-    #doc = Nokogiri::HTML(html)
-    #doc.css("div.exhibitions__featured-list").each do |exhibition|
-    html = open("https://www.montclairartmuseum.org/exhibitions/on-view")
+    html = open("https://www.whitney.org/exhibitions")
     doc = Nokogiri::HTML(html)
-    doc.css("div.view-content").each do |exhibition|
-     binding.pry
+    doc.css("div.exhibition.h1.exhibition__title page-title").each do |exhibition|
+    exhib_first_part = doc.css(".exhibitions__featured-list").text
+    exhib_second_part = doc.css(".exhibitions__list").text
+      binding.pry
+   
+     
       
   end
 end 
   
 end 
+
+
+
+ #html = open("https://www.montclairartmuseum.org/exhibitions/on-view")
+    #doc = Nokogiri::HTML(html)
+    #doc.css("div.view-content").each do |exhibition|
