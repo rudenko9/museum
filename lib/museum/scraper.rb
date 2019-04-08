ffa077dc08efd45db8a20316e849fcdd770a28c7
Nataliya R <n.rudenko@gmail.com>
2019-04-08 16:26:04 +0000

    change scraping

--- lib/museum/scraper.rb
+++ lib/museum/scraper.rb
@@ -4,8 +4,8 @@ class Scraper
     html = open("https://www.whitney.org/exhibitions")
     doc = Nokogiri::HTML(html)
     doc.css("div.h1.exhibition__title page-title").each do |exhibition|
-    exhib_first_part = doc.css(".exhibitions__featured-list").text
-    exhib_second_part = doc.css(".exhibitions__list").text
+    exhib_first_part = exhibition.css(".exhibitions__featured-list").text
+    exhib_second_part = exhibition.css(".exhibitions__list").text
      binding.pry
      
    
