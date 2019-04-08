f6b6ed5822aa7bcf3d452320942bbf83af9b2905
Nataliya R <n.rudenko@gmail.com>
2019-04-08 23:55:50 +0000

    add attr_accessor

--- lib/museum/exhibition.rb
+++ lib/museum/exhibition.rb
@@ -1,3 +1,15 @@
 class Exhibition 
+  attr_accessor :exhibit_first_part, :exhibit_second_part
+  @@all = []
+  def initialize(exhibit_first_part, exhibit_second_part)
+    @exhibit_first_part = exhibit_first_part
+    @exhibit_second_part = exhibit_second_part
+    
+    @@all << self 
+  end
+  
+  def self.all
+    @@all
+  end 
   
 end 
--- lib/museum/scraper.rb
+++ lib/museum/scraper.rb
@@ -6,10 +6,11 @@ class Scraper
     doc.css("div.exhibitions").each do |exhibition|
      binding.pry 
   exhib_first_part = exhibition.css("div.exhibitions__featured-list div.list-item")[0..1]
-    
   exhib_second_part = exhibition.css("div.exhibitions__list div.list-item")[0..1]
      end
 end 
+
+
   
 end 
 
