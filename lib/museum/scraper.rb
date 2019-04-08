e9dd9565e8fad7169aa9e496ce92547507122980
Nataliya R <n.rudenko@gmail.com>
2019-04-08 01:48:13 +0000

    add new webpage

--- lib/museum/scraper.rb
+++ lib/museum/scraper.rb
@@ -1,10 +1,13 @@
 class Scraper 
   
   def scrape_page
-    html = open("https://www.whitney.org/exhibitions")
+    #html = open("https://www.whitney.org/exhibitions")
+    #doc = Nokogiri::HTML(html)
+    #doc.css("div.exhibitions__featured-list").each do |exhibition|
+    html = open("https://www.montclairartmuseum.org/exhibitions/on-view")
     doc = Nokogiri::HTML(html)
-    doc.css("div.exhibitions__featured-list").each do |exhibition|
-      binding.pry
+    doc.css("div.view-content").each do |exhibition|
+     binding.pry
       
   end
 end 
