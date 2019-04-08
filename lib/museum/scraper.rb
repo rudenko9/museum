ceadd7c7993bad5da9f6c98674ab54efc02fd8aa
Nataliya R <n.rudenko@gmail.com>
2019-04-08 19:18:26 +0000

    add more scraping

--- lib/museum/scraper.rb
+++ lib/museum/scraper.rb
@@ -5,21 +5,14 @@ class Scraper
     doc = Nokogiri::HTML(html)
     doc.css("div.exhibitions").each do |exhibition|
      binding.pry 
-    exhib_first_part = exhibition.css("div.exhibitions__featured-list div.list-item")[0..1]
+  exhib_first_part = exhibition.css("div.exhibitions__featured-list div.list-item")[0..1]
     
-    exhib_second_part = exhibition.css(".exhibitions__list").text
-     
-     
-   
-     
-      
-  end
+  exhib_second_part = exhibition.css("div.exhibitions__list div.list-item")[0..1]
+     end
 end 
   
 end 
 
 
 
- #html = open("https://www.montclairartmuseum.org/exhibitions/on-view")
-    #doc = Nokogiri::HTML(html)
-    #doc.css("div.view-content").each do |exhibition|
+ 
