0ef7e1150ef3b32a8bc2d7836caddcc1ba329791
Nataliya R <n.rudenko@gmail.com>
2019-04-09 00:09:05 +0000

    add menu

--- lib/museum/cli.rb
+++ lib/museum/cli.rb
@@ -1,7 +1,15 @@
 class CLI 
   
   def start 
-  puts "hello"
+  puts "Welcome!!!"
+  Scraper.new.scrape_page
+  menu
   end 
+  def menu
+    Exhibition.all.each.with_index(1) do |exhibition, index|
+      puts "#{index}. #{exhibition.exhib_first_part}"
+      puts "#{index}. #{exhibition.exhib_second_part}"
+    end 
+  end
 
 end 
--- lib/museum/scraper.rb
+++ lib/museum/scraper.rb
@@ -4,9 +4,11 @@ class Scraper
     html = open("https://www.whitney.org/exhibitions")
     doc = Nokogiri::HTML(html)
     doc.css("div.exhibitions").each do |exhibition|
-     binding.pry 
+      
+     #binding.pry 
   exhib_first_part = exhibition.css("div.exhibitions__featured-list div.list-item")[0..1]
   exhib_second_part = exhibition.css("div.exhibitions__list div.list-item")[0..1]
+  Exhibition.new(exhib_first_part, exhib_second_part)
      end
 end 
 
