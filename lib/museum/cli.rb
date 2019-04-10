f22aec1f2757b4f47566dfc0f174f588da585460
Nataliya R <n.rudenko@gmail.com>
2019-04-10 23:34:26 +0000

    add

--- lib/museum/cli.rb
+++ lib/museum/cli.rb
@@ -3,17 +3,31 @@ class CLI
  def start 
  puts  "                        WELCOME !!!       "
   Scraper.new.scrape_page
+  list_exhibition
   menu
   end 
   
-  def menu
+  def list_exhibition
     Exhibition.all.each.with_index(1) do |site_name, index|
       #binding.pry
       puts "#{index}. #{site_name.title}" 
-      
-      
-    end 
+      end 
   end
+  
+  def menu
+    puts ""
+    input = gets.strip.downcase
+    if input == "list"
+      list_exhibition
+      menu
+      elsif input == "exit"
+      exit
+      elsif     
+    
+    
+  end   
+  end 
+  
   
   def exit
     puts "Thank You For Visiting Our Museum!"
